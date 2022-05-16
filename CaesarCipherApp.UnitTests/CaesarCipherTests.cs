0bcd2d04ab27038581485f8a4f58cb57c66e3312
JillBirchall <jill.m.birchall@gmail.com>
2022-05-16 17:29:30 +0100

    Adding Unit Tests

---
+++ CaesarCipherApp.UnitTests/CaesarCipherTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using CaesarCipherApp;
+
+
+
+namespace CaesarCipherApp.Tests
+{
+    [TestFixture]
+    public class CaesarCipherTests
+    {
+        [Test]
+        [TestCase("abc", "bcd")]
+        [TestCase("abc def", "bcd efg")]
+        [TestCase("abc-def", "bcd-efg")]
+        [TestCase("xyz", "yza")]
+        public void EncryptMessage_WhenCalled_ReturnEncryptedMessage(string message, string encryptedMessage)
+        {
+            var result = CaesarCipher.EncryptMessage(message, 1);
+            Assert.That(result, Is.EqualTo(encryptedMessage));
+        }
+
+        [Test]
+        [TestCase("bcd", "abc")]
+        [TestCase("bcd efg", "abc def")]
+        [TestCase("bcd-efg", "abc-def")]
+        [TestCase("yza", "xyz")]
+        public void decryptMessage_WhenCalled_ReturnDecryptedMessage(string encryptedMessage, string decryptedMessage)
+        {
+            var result = CaesarCipher.DecryptMessage(encryptedMessage, 1);
+            Assert.That(result, Is.EqualTo(decryptedMessage));
+        }
+
+
+    }
+}
---
+++ CaesarCipherApp.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CaesarCipherApp.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CaesarCipherApp.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6767cff3-2698-4404-b447-399a3200c00d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
