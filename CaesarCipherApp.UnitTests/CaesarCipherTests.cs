8a6682bc4eb11202daa72b5b4c7c1fd3006293a2
JillBirchall <jill.m.birchall@gmail.com>
2022-05-16 17:36:39 +0100

    New test cases added

--- CaesarCipherApp.UnitTests/CaesarCipherTests.cs
+++ CaesarCipherApp.UnitTests/CaesarCipherTests.cs
@@ -10,24 +10,26 @@ namespace CaesarCipherApp.Tests
     public class CaesarCipherTests
     {
         [Test]
-        [TestCase("abc", "bcd")]
-        [TestCase("abc def", "bcd efg")]
-        [TestCase("abc-def", "bcd-efg")]
-        [TestCase("xyz", "yza")]
-        public void EncryptMessage_WhenCalled_ReturnEncryptedMessage(string message, string encryptedMessage)
+        [TestCase("abc", "bcd", 1)]
+        [TestCase("abc def", "bcd efg", 1)]
+        [TestCase("abc-def", "bcd-efg", 1)]
+        [TestCase("xyz", "yza", 1)]
+        [TestCase("Hello", "Rovvy", 10)]
+        public void EncryptMessage_WhenCalled_ReturnEncryptedMessage(string message, string encryptedMessage, int shift)
         {
-            var result = CaesarCipher.EncryptMessage(message, 1);
+            var result = CaesarCipher.EncryptMessage(message, shift);
             Assert.That(result, Is.EqualTo(encryptedMessage));
         }
 
         [Test]
-        [TestCase("bcd", "abc")]
-        [TestCase("bcd efg", "abc def")]
-        [TestCase("bcd-efg", "abc-def")]
-        [TestCase("yza", "xyz")]
-        public void decryptMessage_WhenCalled_ReturnDecryptedMessage(string encryptedMessage, string decryptedMessage)
+        [TestCase("bcd", "abc", 1)]
+        [TestCase("bcd efg", "abc def", 1)]
+        [TestCase("bcd-efg", "abc-def", 1)]
+        [TestCase("yza", "xyz", 1)]
+        [TestCase("Rovvy", "Hello", 10)]
+        public void decryptMessage_WhenCalled_ReturnDecryptedMessage(string encryptedMessage, string decryptedMessage, int shift)
         {
-            var result = CaesarCipher.DecryptMessage(encryptedMessage, 1);
+            var result = CaesarCipher.DecryptMessage(encryptedMessage, shift);
             Assert.That(result, Is.EqualTo(decryptedMessage));
         }
 
